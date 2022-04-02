2493a176f51dae3b87ae4dfc3cf61430aa594f3c
wincall <31054506+wincall@users.noreply.github.com>
2022-04-02 08:16:46 +0800

    pull commit

--- src/main/java/com/athouse/git/GitTest.java
+++ src/main/java/com/athouse/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master test！");
         System.out.println("hot fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
