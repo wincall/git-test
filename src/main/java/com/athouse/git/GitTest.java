9f27024fdfe87427cfa14baad66f9017a37a9fd9
Layne <Layne@atguigu.com>
2022-04-02 08:13:39 +0800

    push commit

--- src/main/java/com/athouse/git/GitTest.java
+++ src/main/java/com/athouse/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master test！");
         System.out.println("hot fix test!");
+        System.out.println("push test!");
     }
 }
